da6504742fa9b00d89a71f9401dfaea49b74964d
trukhachev495 <sergey.trainings@gmail.com>
2023-11-24 19:11:16 +0400

    Decided puppeteer1

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -3,57 +3,57 @@ afterEach(() => {
   page.close();
 });
 
-// describe("Github page tests", () => {
+describe("Github page tests", () => {
 
-//   beforeEach(async () => {
-//     page = await browser.newPage();
-//     await page.goto("https://github.com/team");
-//   });
-//   test("The h1 header content'", async () => {
-//     const firstLink = await page.$("header div div a");
-//     await firstLink.click();
-//     await page.waitForTimeout(3000);
-//     await page.waitForSelector('h1');
-//     const title2 = await page.title();
-//     expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
-//   });
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  });
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForTimeout(3000);
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
+  });
 
-//   test("The first link attribute", async () => {
-//     const actual = await page.$eval("a", link => link.getAttribute('href') );
-//     expect(actual).toEqual("#start-of-content");
-//   });
-//   test("The page contains Sign in button", async () => {
-//     const btnSelector = ".btn-mktg.btn-large-mktg.btn-muted-mktg";
-//     await page.waitForSelector(btnSelector, {
-//       visible: true,
-//     });
-//     const actual = await page.$eval(btnSelector, link => link.textContent);
-//     expect(actual).toContain("Sign up for free")
-//   });
-//     });
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  });
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-mktg.btn-large-mktg.btn-muted-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Sign up for free")
+  });
+    });
 
 
-  // test('The page contains Get started with Team', async() => {
-  //   page = await browser.newPage();
-  //   await page.goto("https://github.com/team");
-  //   const btnSelector1 = ".btn-mktg.flex-sm-self-center.btn-large-mktg";
-  //   await page.waitForSelector(btnSelector1, {
-  //     visible: true,
-  //   });
-  //   const actual = await page.$eval(btnSelector1, link => link.textContent);
-  //   expect(actual).toContain("Get started with Team")
-  // })
-  // test('The page contains Get started with Team', async() => {
-  //   page = await browser.newPage();
-  //   await page.goto("https://github.com/team");
-  //   const btn = ".btn-mktg.flex-sm-self-center.btn-large-mktg";
-  //   await page.click(btn);
-  //   await page.waitForSelector('h1.d-none.d-md-block.mt-0.mb-3.text-center.h1.lh-condensed-ultra');
-  //   // Получаем текст элемента
-  //   const textContent = await page.$eval('h1.d-none.d-md-block.mt-0.mb-3.text-center.h1.lh-condensed-ultra', element => element.textContent);
-  //   // Проверяем, что текст соответствует ожидаемому
-  //   expect(textContent).toEqual("First, let's create your user account");
-  // })
+  test('The page contains Get started with Team', async() => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+    const btnSelector1 = ".btn-mktg.flex-sm-self-center.btn-large-mktg";
+    await page.waitForSelector(btnSelector1, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector1, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  })
+  test('The page contains Get started with Team', async() => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+    const btn = ".btn-mktg.flex-sm-self-center.btn-large-mktg";
+    await page.click(btn);
+    await page.waitForSelector('h1.d-none.d-md-block.mt-0.mb-3.text-center.h1.lh-condensed-ultra');
+    // Получаем текст элемента
+    const textContent = await page.$eval('h1.d-none.d-md-block.mt-0.mb-3.text-center.h1.lh-condensed-ultra', element => element.textContent);
+    // Проверяем, что текст соответствует ожидаемому
+    expect(textContent).toEqual("First, let's create your user account");
+  })
   test('Go to the Actions tab and view the title', async() => {
     page = await browser.newPage();
     await page.goto("https://github.com/team");
